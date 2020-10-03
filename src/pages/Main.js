f3dbc78a6de3ad1bd313582e2b7ebd3ced45d2b4
Jeremy <jeremyVDLRC@gmail.com>
2020-10-03 13:03:48 +1000

    changed render components to card container

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Card } from '../components/Card/Card';
+import { CardContainer } from '../components/Cards/CardContainer/CardContainer';
+
 import { Footer } from '../components/Footer/Footer';
 import { Header } from '../components/Header/Header';
+
 
 export const Main = () =>{
     return(
      <>
-        <Header></Header>
-   <Card  id='about'></Card>
-   <Card id='skills'></Card>
-   <Card id='tracker'></Card>
-    <Footer></Footer>
+  <Header></Header>
+  <CardContainer></CardContainer>
+ <Footer></Footer>
     </>
     )
 }
