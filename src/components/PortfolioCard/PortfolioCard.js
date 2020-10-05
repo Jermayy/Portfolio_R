a1fb85c5a5fbd994bc7f67214f50d13a687ec9ed
Jeremy <jeremyVDLRC@gmail.com>
2020-10-05 14:43:09 +1100

    Portfolio cards centered with dummy content

--- src/components/PortfolioCard/PortfolioCard.js
+++ src/components/PortfolioCard/PortfolioCard.js
@@ -7,19 +7,24 @@ import './PortfolioCard.css'
 export const PortfolioCard = () =>{
 
     return (
-        <div>
-          <div className="card col-sm-8 " id='portfolioCard'>
+     
+          <div className="card col-sm-10 mx-auto" id='portfolioCard'>
           
-
+        <span className='align'>
           <img alt='app thumbnail' src= {img} id='thumbnail'/>
             <div className="card-body" id='cardBody'>
-            <a href='#'>Application</a><br></br>
-            <a href='#'>Github Repo</a>
+                <ul id='portLink'>
+                    <li> <a href='https://github.com/Jermayy'>Application</a></li>
+                    <li><a href='https://github.com/Jermayy'>Github Repo</a></li>
+  
+                </ul>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+
             </div>
-          
+            </span>  
           
           </div>
 
-        </div>
+        
     )
 }
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -4,15 +4,20 @@ import { Footer } from '../components/Footer/Footer';
 import { AltHeader } from '../components/Header/AltHeader';
 import { PortfolioCard } from '../components/PortfolioCard/PortfolioCard';
 
+import './Portfolio.css';
+
 export const Portfolio = () =>{
 
     return(
         <div>
         <AltHeader></AltHeader>
+        <container classname='content'>
         <PortfolioCard></PortfolioCard>
-
-
-
+        <PortfolioCard></PortfolioCard>
+        <PortfolioCard></PortfolioCard>
+        <PortfolioCard></PortfolioCard>
+        <PortfolioCard></PortfolioCard>
+        </container>
         <Footer></Footer>
         </div>
     )
