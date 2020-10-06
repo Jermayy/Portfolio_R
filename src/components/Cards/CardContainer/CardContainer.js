3284cc74f17d82403c2dd7ef9c8d28973d6e4ff5
Jeremy <jeremyVDLRC@gmail.com>
2020-10-06 21:27:40 +1100

    additional styling to card

--- src/components/Cards/CardContainer/CardContainer.js
+++ src/components/Cards/CardContainer/CardContainer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { AboutCard } from '../AboutCard/AboutCard';
 import { SkillCard } from '../SkillCard/SkillCard';
-import { TrackerCard } from '../TrackerCard/TrackerCard';
+
 import './CardContainer.css';
 
 export const CardContainer =()=> {
@@ -9,12 +9,9 @@ export const CardContainer =()=> {
         <div className='body'>   
         <span className='info row'>
        <AboutCard></AboutCard>
-       <div className='col-md-1'></div>
-       <SkillCard></SkillCard>
+        <SkillCard></SkillCard>
         </span>
-        <span className='row'>
-        <TrackerCard></TrackerCard>
-        </span>
+       
         </div>
     )
 }
