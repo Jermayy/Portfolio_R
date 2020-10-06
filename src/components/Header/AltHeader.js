20b2107657a6d517f3ff29edfbacec7dcc6fa663
Jeremy <jeremyVDLRC@gmail.com>
2020-10-06 23:15:17 +1100

    removed buttons and centered content

--- src/components/Header/AltHeader.js
+++ src/components/Header/AltHeader.js
@@ -8,10 +8,10 @@ return(
 <nav className="navbar navbar-inverse">
   <div className="container-fluid">
     <div className="navbar-header">
-      <a className="navbar-brand" href="https://github.com/Jermayy">Jeremy Carlos</a>
+      <a className="navbar-brand" href="/" id='nameLink'>Jeremy Carlos</a>
     </div>
     
-    <button className="btn btn-danger navbar-btn">Contact Me</button>
+    
   </div>
 </nav>
 </div>
