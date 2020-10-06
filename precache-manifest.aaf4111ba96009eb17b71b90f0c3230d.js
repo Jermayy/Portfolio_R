784c1115cbdeb28c18de0dd947a003687521176c
Jeremy <jeremyVDLRC@gmail.com>
2020-10-07 00:25:45 +1100

    Updates

---
+++ precache-manifest.aaf4111ba96009eb17b71b90f0c3230d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e3052ba04292b18e06f1344e8acd7a3",
+    "url": "/Portfolio_R/index.html"
+  },
+  {
+    "revision": "44d22955a47e2e20cd5f",
+    "url": "/Portfolio_R/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "3ade0830c6040f8246ff",
+    "url": "/Portfolio_R/static/css/main.a58a13dd.chunk.css"
+  },
+  {
+    "revision": "44d22955a47e2e20cd5f",
+    "url": "/Portfolio_R/static/js/2.31a9424d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio_R/static/js/2.31a9424d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ade0830c6040f8246ff",
+    "url": "/Portfolio_R/static/js/main.62d33f8e.chunk.js"
+  },
+  {
+    "revision": "fc8909e104c315cf8217",
+    "url": "/Portfolio_R/static/js/runtime-main.6dbb7808.js"
+  },
+  {
+    "revision": "ac8ca2766f30d94ca5a9139f20b58fed",
+    "url": "/Portfolio_R/static/media/MyMVP.ac8ca276.png"
+  },
+  {
+    "revision": "2999a29a2e904d15c34c690e6a0f7860",
+    "url": "/Portfolio_R/static/media/budgetTracker.2999a29a.png"
+  },
+  {
+    "revision": "631df2865fc00937fada115c269ed02d",
+    "url": "/Portfolio_R/static/media/burger.631df286.png"
+  },
+  {
+    "revision": "1c4939ff16cda1187e0835f1666eafe4",
+    "url": "/Portfolio_R/static/media/city.1c4939ff.gif"
+  },
+  {
+    "revision": "48979aecf9b7eb0077fd34819194ea80",
+    "url": "/Portfolio_R/static/media/employeeDirectory.48979aec.png"
+  },
+  {
+    "revision": "583a830d144ecdf70229437f40643ea2",
+    "url": "/Portfolio_R/static/media/fitnessTracker.583a830d.png"
+  },
+  {
+    "revision": "6a046aa01a80c59b52e6c777a4563bfb",
+    "url": "/Portfolio_R/static/media/gamejam.6a046aa0.png"
+  },
+  {
+    "revision": "9f2a9bd2dec5c1253f62d0241c9da739",
+    "url": "/Portfolio_R/static/media/jeremy_headshot.9f2a9bd2.jpg"
+  }
+]);
